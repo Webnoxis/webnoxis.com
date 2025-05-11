f7b4a1b3fbdd6b69a1fc6d81f324e794d1e0f49c
Simeon Petkov <152815802+TheMoni09@users.noreply.github.com>
2025-05-11 15:29:54 +0300

    Update

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,10 +5,30 @@
       <RouterLink to="/" class="font-Poppins font-semibold text-[1.5rem]">Webnoxis</RouterLink>
     </div>
     <div class="flex flex-row items-center justify-items-end gap-x-5 text-[1.1rem] font-Roboto">
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/services">Services</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-      <RouterLink to="/contact">Contact</RouterLink>
+      <RouterLink class="group relative" to="/">
+        Home
+          <span
+            class="absolute left-0 bottom-0 h-[2px] w-0 bg-[#713D86] transition-all duration-300 group-hover:w-full"
+          ></span>
+      </RouterLink>
+      <RouterLink class="group relative" to="/services">
+        Services
+        <span
+            class="absolute left-0 bottom-0 h-[2px] w-0 bg-[#713D86] transition-all duration-300 group-hover:w-full"
+        ></span>
+      </RouterLink>
+      <RouterLink class="group relative" to="/about">
+        About
+        <span
+            class="absolute left-0 bottom-0 h-[2px] w-0 bg-[#713D86] transition-all duration-300 group-hover:w-full"
+        ></span>
+      </RouterLink>
+      <RouterLink class="group relative" to="/contact">
+        Contact
+        <span
+            class="absolute left-0 bottom-0 h-[2px] w-0 bg-[#713D86] transition-all duration-300 group-hover:w-full"
+        ></span>
+      </RouterLink>
     </div>
   </div>
 </template>
