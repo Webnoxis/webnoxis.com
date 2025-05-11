3f7ee36c3175598ff2322563f8f2bd459bec9c8c
xnuzz <tsvetkovalex790@gmail.com>
2025-05-11 15:28:26 +0300

    Update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,7 @@ import WelcomeItem from "../components/WelcomeItem.vue"
 import Services from "../components/Services.vue"
 import About from "../components/About.vue"
 import Partners from "../components/Partners.vue"
+import Contact from "../components/Contact.vue"
 
 // Define the refs with proper types
 const aboutRef = ref<InstanceType<typeof About> | null>(null)
@@ -27,5 +28,6 @@ function scrollToServices() {
     <Services ref="servicesRef"/>
     <About ref="aboutRef"/>
     <Partners />
+    <Contact />
   </main>
 </template>
